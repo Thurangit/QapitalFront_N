c7cb3a6d78c14ba52df0626e8e0857680ae0ad06
ThuranJunior <thuransabba.ing@gmail.com>
2025-12-27 13:16:36 +0100

    MA

--- src/modules/urlApp.js
+++ src/modules/urlApp.js
@@ -1,7 +1,7 @@
 
 
 export const urlApp = "https://gifty-cm.vercel.app/";
-export const urlPublicAPi = `http://127.0.0.1:8003`;
+export const urlPublicAPi = `http://10.127.163.237:8000`;
 export const urlServerImage = urlPublicAPi;
 export const urlApi = `${urlPublicAPi}/api`;
 
